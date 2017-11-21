9e78756d7cd8a67ef0ce452f1d81be14a1dc9a1b
Hasoo Kim <vaxzeem@i-heart.co.kr>
2017-11-22 03:02:30 +0900

    modified DtailView

--- board/urls.py
+++ board/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.WorkflowListView.as_view(), name='workflow-list'),
-    url(r'workflow/(?P<pk>[0-9]+)/$', views.WorkflowDetailView.as_view(), name='workflow-detail'),
+    url(r'^$', views.WorkflowListView.as_view(), name='workflow-list'),
+    url(r'^workflow/(?P<pk>[0-9]+)/$', views.WorkflowDetailView.as_view(), name='workflow-detail'),
 ]
--- board/views.py
+++ board/views.py
@@ -3,16 +3,9 @@ from django.views.generic.list import ListView
 from django.views.generic import DetailView
 from board.models import Workflow
 
-def iw_list(request):
-    return render(request, 'board/iw_page.html')
-
 class WorkflowListView(ListView):
 
     model = Workflow
-
-    def get_context_data(self, **kwargs):
-        context = super(WorkflowListView, self).get_context_data(**kwargs)
-        return context
 
 class WorkflowDetailView(DetailView):
 
