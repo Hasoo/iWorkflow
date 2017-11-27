4c958d44388ebacc0a2c32ab5af314ed3609a4f8
Hasoo <vaxzeen@hotmail.com>
2017-11-27 23:29:56 +0900

    added http 401 error code

--- board/views.py
+++ board/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
@@ -11,15 +12,33 @@ class WorkflowListView(ListView):
     paginate_by = 3
     ordering = ['-seq']
 
+    def dispatch(self, *args, **kwargs):
+        if None == self.request.session.get('member_id'):
+            return HttpResponse('401 Unauthorized', status=401)
+            #return redirect('login')
+        return super(WorkflowListView, self).dispatch(*args, **kwargs)
+
 class WorkflowDetailView(DetailView):
 
     model = Workflow
+
+    def dispatch(self, *args, **kwargs):
+        if None == self.request.session.get('member_id'):
+            return HttpResponse('401 Unauthorized', status=401)
+            #return redirect('login')
+        return super(WorkflowDetailView, self).dispatch(*args, **kwargs)
 
 class WorkflowCreate(CreateView):
 
     model = Workflow
     success_url = reverse_lazy('workflow-list')
     fields = ['status', 'title', 'desc', 'attach', 'secret_key']
+
+    def dispatch(self, *args, **kwargs):
+        if None == self.request.session.get('member_id'):
+            return HttpResponse('401 Unauthorized', status=401)
+            #return redirect('login')
+        return super(WorkflowCreate, self).dispatch(*args, **kwargs)
 
     def form_valid(self, form):
         form.instance.worker = self.request.session['member_id']
@@ -29,6 +48,12 @@ class WorkflowUpdate(UpdateView):
 
     model = Workflow
     fields = ['status', 'title', 'desc', 'attach', 'secret_key']
+
+    def dispatch(self, *args, **kwargs):
+        if None == self.request.session.get('member_id'):
+            return HttpResponse('401 Unauthorized', status=401)
+            #return redirect('login')
+        return super(WorkflowUpdate, self).dispatch(*args, **kwargs)
 
     def get_success_url(self):
         return reverse('workflow-detail', args=(self.object.pk,))
