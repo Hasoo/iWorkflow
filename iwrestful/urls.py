0fd010a5c44f681cbe6c6bfed009d730e60233bf
Hasoo <vaxzeen@hotmail.com>
2017-12-08 17:23:00 +0900

    added UpdateAPIView, partial_update

--- iwrestful/urls.py
+++ iwrestful/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.workflow_api.as_view()),
+    url(r'^$', views.WorkflowListAPI.as_view()),
+    url(r'^update/(?P<pk>[0-9]+)$', views.WorkflowUpdateAPI.as_view()),
 ]
